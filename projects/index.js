ce845d16aea271f94bdcd0d9401bd19f91717fd9
ianlee2024 <ian.lee@zealitconsultants.com>
2024-06-13 11:33:38 -0500

    new homepage

--- projects/index.js
+++ projects/index.js
@@ -15,3 +15,14 @@ window.addEventListener('scroll', function() {
         header.style.backgroundColor = 'rgba(51, 51, 51, 0.8)'; // Original opacity when not scrolling
     }
 });
+const bubble = document.getElementById('hoverBubble');
+        const originalText = "Our Promise";
+        const hoverText = "At Base, we're dedicated to simplifying and enhancing everyday experiences through intuitive technology. Our suite of apps - a tip calculator, grocery list organizer, rock-paper-scissors game, and weather companion - is designed to seamlessly integrate into your routine, empowering you to make informed decisions, stay organized, and add a touch offun to your day. We strive to provide practical solutions that effortlessly fit into your lifestyle, ensuring that with Base, you have the essential tools at your fingertips to navigate through life's moments with ease and enjoyment.";
+
+        bubble.addEventListener('mouseenter', () => {
+            bubble.textContent = hoverText;
+        });
+
+        bubble.addEventListener('mouseleave', () => {
+            bubble.textContent = originalText;
+        });
